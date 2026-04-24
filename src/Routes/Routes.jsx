81450970a94948b21176e7fda688bbbcd1b831d3
leadav11 <lportocarrerocr@ucvvirtual.edu.pe>
2026-04-23 22:09:28 -0500

    fix router github pages

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 // Importación de páginas
 import Quizz from "../Components/Quizz";
