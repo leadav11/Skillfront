418d1172cf598c146effc4c887e60a3a872c83a4
leadav11 <lportocarrerocr@ucvvirtual.edu.pe>
2026-04-23 22:16:45 -0500

    cambios2

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import Lea from '.././assets/Images/Lea.jpg';
+import Lea from '.././assets/Images/Lea.png';
 
 export default function Profile({ onProfileUpdate }) {
   const [profileData, setProfileData] = useState({
